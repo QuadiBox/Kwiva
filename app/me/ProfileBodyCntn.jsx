c6ea8adbdac506f5e450bb59e1886758190cfe91
Dojah <oladojaabdquadridamilola@gmail.com>
2025-08-22 20:25:10 +0100

    updated middleware

--- app/me/ProfileBodyCntn.jsx
+++ app/me/ProfileBodyCntn.jsx
@@ -421,7 +421,7 @@ const ProfileBodyCntn = () => {
 
     const isPartOfWinners = preview?.findIndex((elem) => elem?.user_id === user?.id);
     const pointfromFirst = parseInt(preview[0]?.points) - parseInt(monthlyPoints);
-    const pointfromTopWinners = parseInt(preview[0]?.points) - parseInt(monthlyPoints)
+    const pointfromTopWinners = parseInt(preview[9]?.points) - parseInt(monthlyPoints)
 
     return (
         <div className="profileBodyCntn">
