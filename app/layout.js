d429ae93f45325fdf95ace45fdb7ed419affd6b1
Dojah <oladojaabdquadridamilola@gmail.com>
2025-08-24 10:15:27 +0100

    more ad test with infolinks

--- app/layout.js
+++ app/layout.js
@@ -92,6 +92,7 @@ export default function RootLayout({ children }) {
             });
           `}
           </Script>
+          <script type="text/javascript"> var infolinks_pid = 3439488; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
         </body>
       </html>
     </ClerkProvider>
